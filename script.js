237cf14ad035c9715a6ca37aa3ebb806594b8549
Nasim Akbor <nasimakbor@Nasims-Air.fios-router.home>
2022-03-21 01:30:04 -0400

    big fixes

--- script.js
+++ script.js
@@ -1,17 +1,20 @@
 //Code
 
+//Elements needed
 let grid = document.querySelector('.gameBox');
 let start = document.querySelector('#start-screen');
+let startBtn = document.querySelector('.start');
 let end = document.querySelector('#game-over');
 let win = document.querySelector('#winner');
 let result = document.querySelector('.scoreNum');
 let score = parseInt(result.innerText);
 let playerIndex = 351; //Starting position of player
-let blasterIndex = playerIndex - 19; // Spot where fired shot will move
+let blasterIndex = playerIndex - 19; // Spot where fired shot will start
 let moveDownRight = true;
 let moveDownLeft = true;
 let deadBaddies = [];
 
+//Fill game play area with divs
 for (let i = 0; i < 380; i++) {
   let boxes = document.createElement('div');
   grid.append(boxes);
@@ -27,18 +30,19 @@ let spots = [ //Positions within allDivs that I want the aliens to start at. 11x
   61, 62, 63, 64, 65, 66, 67, 68, 69, 70, 71,
   80, 81, 82, 83, 84, 85, 86, 87, 88, 89, 90
 ]
+//Function to draw in the aliens.
 function makeEm() {
   for (let i = 0; i < spots.length; i++) {
     if (!deadBaddies.includes(i))
       allDivs[spots[i]].classList.add('nondescriptLifeform');
-
   }
   allDivs[playerIndex].classList.add('playerUn');
 }
 
-let leftSide = spots[0] % 19 === 0;
-let rightSide = spots[10] % 19 === 18;
-let dirCount = 1;
+let dirCount; //Determines direction of alien movement.
+let rand = Math.random() < 0.5 ? dirCount = 1 : dirCount = 2;
+
+//Controls for Player One. 
 document.addEventListener('keydown', (e) => {
   if (e.code === 'ArrowLeft' && playerIndex > 342) {
     allDivs[playerIndex].classList.remove('playerUn');
@@ -49,10 +53,12 @@ document.addEventListener('keydown', (e) => {
     playerIndex++;
     allDivs[playerIndex].classList.add('playerUn');
   } else if (e.code === 'Space' || e.code === 'ArrowUp') {
-    setTimeout(shootEmDown(), 2000);
+    e.preventDefault();
+    shootEmDown();
   }
 })
 
+//Function that determines alien movement.
 function moveBaddies() {
   makeEm();
 
@@ -62,6 +68,7 @@ function moveBaddies() {
       allDivs[spots[i]].classList.remove('nondescriptLifeform');
       spots[i] += 20;
       dirCount = 2;
+      checkBottomEnd();
       makeEm();
       moveDownRight = false;
     }
@@ -72,6 +79,7 @@ function moveBaddies() {
     for (let i = 0; i < spots.length; i++) {
       allDivs[spots[i]].classList.remove('nondescriptLifeform');
       spots[i] += 18;
+      checkBottomEnd();
       makeEm();
       moveDownLeft = false;
       dirCount = 1;
@@ -97,21 +105,33 @@ function moveBaddies() {
     }
   }
 
-  //Game Over if player hit
+  //Game Over if player hit by alien.
   if (allDivs[playerIndex].classList.contains('playerUn' && 'nondescriptLifeform')) {
-    console.log('Game Over');
     clearInterval(runGame);
     grid.style.display = 'none';
     end.style.display = 'flex';
   }
+  //Game Over if aliens hit bottom of screen.
+  function checkBottomEnd() {
+    for (let i = 0; i < spots.length; i++) {
+      if ((spots[i] + 19) > 380 && allDivs[spots[i]].classList.contains('nondescriptLifeform')) {
+        clearInterval(runGame);
+        grid.style.display = 'none';
+        end.style.display = 'flex';
+      }
+    }
+  }
 
+  //Win screen if all aliens removed.
   if (deadBaddies.length === spots.length) {
     grid.style.display = 'none';
     win.style.display = 'flex';
     clearInterval(runGame);
   }
+
 }
 
+//function for creating and moving the blaster.
 function shootEmDown() {
 
   let shootingInterval = setInterval(moveBlaster, 300)
@@ -124,32 +144,27 @@ function shootEmDown() {
       clearInterval(shootingInterval);
       setTimeout(() => allDivs[currentBlaster].classList.remove('pewpew'), 50)
     }
+    //When blaster hits a alien it removes the alien&blaster
     if (allDivs[currentBlaster].classList.contains('pewpew' && 'nondescriptLifeform')) {
       allDivs[currentBlaster].classList.add('death');
       allDivs[currentBlaster].classList.remove('pewpew');
       allDivs[currentBlaster].classList.remove('nondescriptLifeform');
-      score += 10;
+      score += 10; //10 pts per alien killed
       result.innerText = score;
 
       clearInterval(shootingInterval); //Stops blaster from continuing.
       setTimeout(() => allDivs[currentBlaster].classList.remove('death'), 100) //Removes death mark.
 
       let newDeath = spots.indexOf(currentBlaster);
-      console.log(newDeath);
       deadBaddies.push(newDeath);
-      console.log(deadBaddies);
     }
-
   }
 }
-
-let runGame;
 
 function startGame() {
   start.style.display = 'none';
   grid.style.display = 'flex';
-  runGame = setInterval(moveBaddies, 500);
+  runGame = setInterval(moveBaddies, 250);
 }
 
-start.addEventListener('click', startGame);
-
+startBtn.addEventListener('click', startGame);
